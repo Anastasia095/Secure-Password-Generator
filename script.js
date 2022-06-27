41a9588e0226a169f172305ccfe147d08de00418
Anastasia Tarasenko <anastas.tarasenko001@mymdc.net>
2022-06-26 21:07:51 -0400

    fixed issues

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 // Assignment code here
 
-
 // Get references to the #generate element
 generateBtn = document.querySelector("#generate");
 
@@ -10,13 +9,9 @@ function generatePassword() {
   var U = 0;
   var N = 0;
   var S = 0;
-  var min = 0;
   var max = 0;
   var password = [];
-  var selectionCounter = 0;
   var j = 0;
-  var leftToGenerate = 0;
-  var getRandom = 0;
 
   function getUserSelection() {
     L = confirm("include lowercase?");
@@ -29,93 +24,72 @@ function generatePassword() {
       }
     
   }
-
-  function counter() {
-    var count = 0;
-    if (L) {
-      count++;
-    }
-
-    if (U) {
-      count++;
-    }
   
-    if (N) {
-      count++;
-    }
-  
-    if (S) {
-      count++;
-    }
-  
-    return count;
-  }
+  getUserSelection();
 
-  getUserSelection() ;
-  selectionCounter = counter();
-
-  if (L) {
-    function generateLower() {
-    // Returns a random integer from 97 to 122 https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
-    password[j] = Math.floor(Math.random() * (122 - 97 + 1) + 97);
-    // converts num to UTF-8 https://www.w3schools.com/html/html_charset.asp
-    password[j] = String.fromCharCode(password[j]);
-    j++
-    }
-    generateLower();
-  }
-  if (U) {
-    function generateUpper() {
-      password[j] = Math.floor(Math.random() * (90 - 65 + 1) + 65);
+  while (password.length < max)
+  {
+    if (L == true && password.length < max) {
+      function generateLower() {
+      // Returns a random integer from 97 to 122 https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+      password[j] = Math.floor(Math.random() * (122 - 97 + 1) + 97);
+      // converts num to UTF-8 https://www.w3schools.com/html/html_charset.asp
       password[j] = String.fromCharCode(password[j]);
       j++
-    }
-    generateUpper();
-  }
-  if (N) {
-    function generateNum() {
-      password[j] = Math.floor(Math.random() * (9 - 0 + 1) + 0);
-      j++
-    }
-    generateNum();
-  }
-  if (S) {
-    function generateSymbol(){
-      const symbols = '!#$%&()*+,-./:;<=>?@[\]^_{|}~';
-      //that will generate random number to land  on random array cell in symbols
-      password[j] = symbols[Math.floor(Math.random() * symbols.length)];
-      j++;
-    }
-    generateSymbol();
-  }
-  
-  leftToGenerate = max - selectionCounter;
-
-  for (var index = 0; index < leftToGenerate; index++) {
-    //there are 4 categories of characters, at least 1 will be selected, this is why max random value is set to 3 (randomly generating char from the 3 categories that are left
-    getRandom = Math.floor(Math.random() * (3 - 0 + 1) + 0);
-    if (getRandom == 0){
-      if (L) 
-        generateLower();
-    }
-    else if (getRandom == 1) {
-      if (U){
-        generateUpper();
       }
+      generateLower();
     }
-    else if (getRandom == 1) {
-      if (N){
-        generateNum();
+    if (U == true && password.length < max) {
+      function generateUpper() {
+        password[j] = Math.floor(Math.random() * (90 - 65 + 1) + 65);
+        password[j] = String.fromCharCode(password[j]);
+        j++
       }
+      generateUpper();
     }
-    else {
-      if (S) 
+    if (N == true && password.length < max) {
+      function generateNum() {
+        password[j] = Math.floor(Math.random() * (9 - 0 + 1) + 0);
+        j++
+        
+      }
+      generateNum();
+    }
+    if (S == true && password.length < max) {
+      function generateSymbol(){
+        const symbols = '!#$%&()*+,-./:;<=>?@[\]^_{|}~';
+        //that will generate random number to land  on random array cell in symbols
+        password[j] = symbols[Math.floor(Math.random() * symbols.length)];
+        j++;
+      }
       generateSymbol();
     }
-
-
   }
-  //converting array inso string with no commas
+
+  //********************Fisher–Yates shuffle to shuffle characters in password array (c) code courtesy of stack overflow =D*/ 
+  function shuffle(array) {
+    var currentIndex = array.length,  randomIndex;
+  
+    // While there remain elements to shuffle.
+    while (currentIndex != 0) {
+  
+      // Pick a remaining element.
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex--;
+  
+      // And swap it with the current element.
+      [array[currentIndex], array[randomIndex]] = [
+        array[randomIndex], array[currentIndex]];
+    }
+  
+    return array;
+  }
+  
+  //******************** */
+
+  console.log("Test password befor convertion" + password);
+  shuffle(password);
+  console.log("Test password befor convertion" + password);
   var generatedPassword = password.join("");
   console.log("Generated password test: " + password);
   console.log("Generated password test: " + generatedPassword);
@@ -123,6 +97,7 @@ function generatePassword() {
   return generatedPassword;
 }
 
+ 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
